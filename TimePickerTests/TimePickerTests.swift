7468172694f0e9851245171384248b93cfaeb3a0
Oleh Stasula <oleh.stasula@gmail.com>
2017-04-23 19:15:12 +0300

    Sample test

--- TimePickerTests/TimePickerTests.swift
+++ TimePickerTests/TimePickerTests.swift
@@ -6,30 +6,16 @@
 //  Copyright © 2017 Oleh Stasula. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
+import TimePicker
 
-class TimePickerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class TimePickerTests: QuickSpec {
+    override func spec() {
+        describe("TimePicker") {
+            it("works") {
+                expect(true).to(beTrue())
+            }
         }
     }
-    
 }
